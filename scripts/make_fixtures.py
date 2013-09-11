caa73a69d814c2be341a6c9f38dc9e444d895adb
Jeferson Farias Calazans <calazans10@gmail.com>
2013-09-10 21:57:15 -0300

    melhorando script de make_fixtures

--- scripts/make_fixtures.py
+++ scripts/make_fixtures.py
@@ -52,21 +52,64 @@ def make_employee(first_name, last_name, phone, birthday_date, department,
 
 
 def make_data():
-    make_department('Comercial')
-    make_department('Financeiro')
+    comercial = make_department('Comercial')
+    financeiro = make_department('Financeiro')
     rh = make_department('Recursos Humanos')
-    make_department('Jurídico')
-    make_department('TI')
+    juridico = make_department('Jurídico')
+    ti = make_department('TI')
 
-    make_job_position('CEO')
-    make_job_position('CTO')
+    ceo = make_job_position('CEO')
+    cto = make_job_position('CTO')
     gerente = make_job_position('Gerente')
-    make_job_position('Desenvolvedor')
-    make_job_position('Designer')
-    make_job_position('Operacional')
+    dev = make_job_position('Desenvolvedor')
+    designer = make_job_position('Designer')
+    operacional = make_job_position('Operacional')
 
     birthday_date = date(1985, 10, 20)
-    make_employee('Paula', 'Rangel', '2185943327', birthday_date, rh, gerente)
+    paula = make_employee('Paula', 'Rangel', '2185943327', birthday_date, rh,
+                          gerente)
+    paula.user.is_staff = True
+    paula.user.is_superuser = True
+    paula.user.save()
+
+    birthday_date = date(1986, 06, 14)
+    roberto = make_employee('Manuel', 'Conceição', '2187353320', birthday_date,
+                            juridico, operacional)
+
+    birthday_date = date(1987, 05, 27)
+    roberto = make_employee('Rosana', 'Bastos', '2192353327', birthday_date,
+                            juridico, gerente)
+
+    birthday_date = date(1988, 06, 07)
+    roberto = make_employee('Camila', 'Costa', '2182353329', birthday_date,
+                            financeiro, operacional)
+
+    birthday_date = date(1984, 01, 10)
+    roberto = make_employee('Adélia', 'da Guia', '2194663327', birthday_date,
+                            financeiro, gerente)
+
+    birthday_date = date(1987, 05, 15)
+    roberto = make_employee('Roberto', 'Silva', '2185943317', birthday_date,
+                            rh, operacional)
+    roberto.user.is_staff = True
+    roberto.user.is_superuser = True
+    roberto.user.save()
+
+    birthday_date = date(1990, 02, 25)
+    make_employee('Felipe', 'Magalhães', '218594397', birthday_date,
+                  comercial, ceo)
+
+    birthday_date = date(1988, 05, 15)
+    make_employee('Mauro', 'Lima', '218894397', birthday_date,
+                  ti, cto)
+
+    birthday_date = date(1990, 07, 25)
+    make_employee('Lilian', 'Ramos', '218694397', birthday_date,
+                  ti, designer)
+
+    birthday_date = date(1986, 01, 01)
+    make_employee('Alberto', 'Brandão', '218694396', birthday_date,
+                  ti, dev)
 
 
 def run():
