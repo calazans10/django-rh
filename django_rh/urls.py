4d9e7e085656b450a1b3d4770c8eae94c6c8fa46
Jeferson Farias Calazans <calazans10@gmail.com>
2013-09-17 14:26:39 -0300

    some conf changes

--- django_rh/urls.py
+++ django_rh/urls.py
@@ -9,10 +9,6 @@ admin.autodiscover()
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'rh_system.views.home', name='home'),
-    # url(r'^rh_system/', include('rh_system.foo.urls')),
-
     url(r'^admin/lookups/', include(ajax_select_urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
